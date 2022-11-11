6db0b0156b6a45232d1bd6468bd5677ea87d404f
Raffael28 <rafael.oliveira.aut18@gmail.com>
2022-11-11 16:13:51 -0300

    Conditional Exercise: 1

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,21 +9,25 @@ namespace CSharpFundamentals
 	{
 		static void Main(string[] args)
 		{
-			var season = Seasons.Autumn;
-
-			switch (season)
+			Console.WriteLine("Please, enter a number between 1 to 10:");
+			var number = 0;
+			try
 			{
-				case Seasons.Autumn:
-				case Seasons.Summer:
-					Console.WriteLine("We've got a promotion");
-					break;
-				case Seasons.Spring:
-					Console.WriteLine("It's spring.");
-					break;
-				default:
-					Console.WriteLine("It's time to make a hot chocolate");
-					break;
+				 number = (Convert.ToInt32(Console.ReadLine()));
+				 if (number > 1 && number < 10)
+					 Console.WriteLine("Valid");
+				 else
+				 {
+					 Console.WriteLine("Invalid");
+				 }
 			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Invalid");
+				
+			}
+
+			
 
 		}
 
