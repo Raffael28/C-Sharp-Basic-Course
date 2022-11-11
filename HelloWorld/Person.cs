70123fdb5839cd3fd200de3c9d641e94a9c75946
Raffael28 <rafael.oliveira.aut18@gmail.com>
2022-11-11 14:03:20 -0300

    Add project files.

---
+++ HelloWorld/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+	public class Person
+	{
+		public string firstName;
+		public string lastName;
+
+		public void Introduce()
+		{
+			Console.WriteLine("Hello, i'm "+ firstName +" "+lastName);
+		}
+	}
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using HelloWorld.Math;
+
+namespace HelloWorld
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var rafael = new Person();
+		
+			rafael.firstName = "Rafael";
+			rafael.lastName = "Oliveira";
+			rafael.Introduce();
+			Calculator calculator = new Calculator();
+			var result = calculator.Add(1, 2);
+			Console.WriteLine(result);
+
+		}
+	}
+}
