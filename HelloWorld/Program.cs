cc2e1369b89bd6c6af6e831835cb9f38d61f1408
Raffael28 <rafael.oliveira.aut18@gmail.com>
2022-11-11 15:43:23 -0300

    Conditional Operator Example 1

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,32 +5,24 @@ using System.Runtime.InteropServices;
 
 namespace CSharpFundamentals
 {
-	public class Person
-	{
-		public int Age;
-	}
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			var number = 1;
-			Increment(number);
-			Console.WriteLine(number);
-			var person1 = new Person();
-			person1.Age = 20;
-			MadeOlder(person1);
-			Console.WriteLine(person1.Age);
+			int hour = 18;
+			if (hour > 0 && hour < 12)
+			{
+
+				Console.WriteLine("It's Morning.");
+			}
+			else if (hour >= 12 && hour < 18)
+				Console.WriteLine("It's afternoon.");
+			else
+				Console.WriteLine("It's evening.");
 
 		}
 
-		public static void Increment(int number)
-		{
-			number += 10;
-		}
-
-		public static void MadeOlder(Person person)
-		{
-			person.Age += 10;
-		}
+		
 	}
 }
