4b9fab2980be4956953cdef44568893d9e2e00a4
Raffael28 <rafael.oliveira.aut18@gmail.com>
2022-11-11 16:57:08 -0300

    For loop

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,7 +1,5 @@
 ﻿
 using System;
-using System.Net;
-using System.Runtime.InteropServices;
 
 	
 namespace CSharpFundamentals
@@ -10,38 +8,17 @@ namespace CSharpFundamentals
 	{
 		static void Main(string[] args)
 		{
-			int speedLimit;
-			int carSpeed;
-			try
+			for (var i = 0; i <= 10; i++)
 			{
-				Console.WriteLine("Please, enter the speed limit (km/h):");
-
-				speedLimit = (Convert.ToInt32(Console.ReadLine()));
-
-				Console.WriteLine("Please, the car speed (km/h):");
-
-				carSpeed = (Convert.ToInt32(Console.ReadLine()));
-				var demeritPoints = (carSpeed - speedLimit) / 5;
-				if (speedLimit >= carSpeed)
-				{
-					Console.WriteLine("OK");
-				}
-				else if (carSpeed > speedLimit && demeritPoints<=12)
-				{
-					Console.WriteLine($"The car speed is above the limit, you take {demeritPoints} demerit points");
-				}
-				else
-				{
-					Console.WriteLine("License Suspended");
-
-				}
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine("You need to insert a valid number!");
-				
+				if (i % 2 == 0)
+					Console.WriteLine(i);
 			}
 
+			for (var i = 10; i > 1; i--)
+			{
+				if(i%2==0) 
+					Console.WriteLine(i);
+			}
 			
 
 		}
