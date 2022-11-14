b6a13fb42e7b0f9e388ea338f15cbd7ad88da589
Raffael28 <rafael.oliveira.aut18@gmail.com>
2022-11-14 13:18:28 -0300

    Demo: arrays

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,24 +8,52 @@ namespace CSharpFundamentals
 	{
 		static void Main(string[] args)
 		{
+			var numbers = new [] { 1, 8, 7, 22, 32, 9 };
 
-		
-				Console.WriteLine("Enter a series of numbers separated by a comma:");
-				var n = Console.ReadLine();
-				var numberArray = n.Split(',');
-				var max = 0;
-				for (int i = 0; i < numberArray.Length; i++)
-				{
-					if (Convert.ToInt32(numberArray[i]) > max)
-					{
-						max = Convert.ToInt32(numberArray[i]);
-					}
-				}
+			//Length
+			Console.WriteLine($"length: {numbers.Length}");
 
-				Console.WriteLine($"The biggest number is {max}");
+			//IndexOf()
+			Console.WriteLine($"Index of 9: {Array.IndexOf(numbers, 22)}");
 
+			//Clear()
+			Array.Clear(numbers, 0,3);
+			Console.WriteLine("Effect of Clear()");
+			foreach (var number in numbers)
+			{
+				Console.WriteLine(number);
+			}
+
+			//Copy()
+
+			var newNumbers = new int[4];
+			Array.Copy(numbers, newNumbers, 4);
+			Console.WriteLine("Effect of Copy()");
+			foreach (var number in newNumbers)
+			{
+				Console.WriteLine(number);
+			}
+
+			//Sort()
+			Array.Sort(numbers);
+			Console.WriteLine("Effect of Sort()");
+
+			foreach (var number in numbers)
+			{
+				Console.WriteLine(number);
+			}
+
+			//Reverse()
+
+			Array.Reverse(numbers);
+			Console.WriteLine("Effect of Reverse()");
+
+			foreach (var number in numbers)
+			{
+				Console.WriteLine(number);
+			}
 		}
 
-		
+
 	}
 }
