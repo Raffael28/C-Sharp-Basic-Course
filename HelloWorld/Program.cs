d038e43f28bfb6092de23400aebab18424f6acc0
Raffael28 <rafael.oliveira.aut18@gmail.com>
2022-11-14 18:32:01 -0300

    StringBuilder

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,8 +1,6 @@
-﻿
-
-
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Text;
 using HelloWorld;
 
 namespace CSharpFundamentals
@@ -11,10 +9,21 @@ namespace CSharpFundamentals
 	{
 		static void Main()
 		{
-			var sentence =
-				"This is going to be a really really really really really really really really really really long text";
-			var summary = StringUtility.SummerizeText(sentence, 25);
-			Console.WriteLine(summary);
+			var builder = new StringBuilder("Hello world");
+			builder
+				.Append('-', 10)
+				.AppendLine()
+				.Append("Header")
+				.AppendLine()
+				.Append('-', 10)
+				.Replace('-', '+')
+				.Remove(0, 10)
+				.Insert(0, new string('-', 10));
+
+			Console.WriteLine(builder);
+
+			Console.WriteLine("First Char: " + builder[0]);
+
 
 		}
 
