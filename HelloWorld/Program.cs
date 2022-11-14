9d4f9a7fd10f0e07ac643074dbcf5c8f5faef122
Raffael28 <rafael.oliveira.aut18@gmail.com>
2022-11-14 18:21:32 -0300

    Summarising Text

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,6 +2,8 @@
 
 
 using System;
+using System.Collections.Generic;
+using HelloWorld;
 
 namespace CSharpFundamentals
 {
@@ -9,35 +11,14 @@ namespace CSharpFundamentals
 	{
 		static void Main()
 		{
-			var fullName = "Rafael Oliveira ";
-			Console.WriteLine($"Trim: {fullName.Trim()}");
-			Console.WriteLine($"ToUpper: {fullName.ToUpper()}");
-
-			var index = fullName.IndexOf(" ");
-			var firstName = fullName.Substring(0, index);
-			var lastName = fullName.Substring(index + 1);
-			Console.WriteLine($"firstName: {firstName}");
-			Console.WriteLine($"lastName: {lastName}");
-
-			var names = fullName.Split(' ');
-			Console.WriteLine($"firstName: {names[0]}");
-			Console.WriteLine($"lastName: {names[1]}");
-
-			Console.WriteLine(fullName.Replace("Oliveira", "Rodrigues"));
-
-			if (String.IsNullOrWhiteSpace(" "))
-				Console.WriteLine("Invalid");
-
-			var str = "25";
-			var age = Convert.ToByte(str);
-			Console.WriteLine(age);
-
-			float price = 19.95f;
-			Console.WriteLine(price.ToString("C0"));
-
+			var sentence =
+				"This is going to be a really really really really really really really really really really long text";
+			var summary = StringUtility.SummerizeText(sentence, 25);
+			Console.WriteLine(summary);
 
 		}
 
+	
 
 	}
 }
